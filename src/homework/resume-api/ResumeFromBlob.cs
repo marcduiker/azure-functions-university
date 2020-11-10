7fdf36e3a9216454fc6ae1e20725491055869eaf
Marc Duiker <marcduiker@gmail.com>
2020-11-10 21:44:33 +0100

    Formatting

--- src/homework/resume-api/ResumeFromBlob.cs
+++ src/homework/resume-api/ResumeFromBlob.cs
@@ -13,23 +13,24 @@ namespace AzureFunctionsUniversity.HomeWork
     {
         [FunctionName("ResumeFromBlob")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, nameof(HttpMethods.Get), Route = null)] HttpRequest req,
+            [HttpTrigger(
+                AuthorizationLevel.Anonymous, 
+                nameof(HttpMethods.Get), 
+                Route = null)] HttpRequest req,
             [Blob("resume/resume.json", FileAccess.Read)] Stream resumeStream, 
             ILogger log)
         {
-
             IActionResult result;
             using var reader = new StreamReader(resumeStream);
-                var content = await reader.ReadToEndAsync();
-                result = new ContentResult 
-                { 
-                    Content = content,
-                    ContentType = MediaTypeNames.Application.Json,
-                    StatusCode = 200
-                };
-                return result;
+            var content = await reader.ReadToEndAsync();
+            result = new ContentResult 
+            { 
+                Content = content,
+                ContentType = MediaTypeNames.Application.Json,
+                StatusCode = 200
+            };
+
+            return result;
         }
     }
-
-   
 }
