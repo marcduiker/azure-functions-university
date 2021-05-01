8d312ff5065442733afa1dfe351b7cd245602051
Christian Lechner <lechnerc77@users.noreply.github.com>
2021-05-01 20:15:23 +0200

    typescript blob - wo tour

---
+++ src/typescript/AzureFunctions.Blob/05UsingPlainBlobInputBindings/GetPlayerFromBlob/index.ts
@@ -0,0 +1,32 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+
+    let responseStatusCode: number
+    let responseMessage: string
+    let responseHeaders: object
+
+    if (context.bindings.playerBlobIn) {
+
+        responseStatusCode = 200
+        responseHeaders = { "Content-Type": "application/json" }
+        responseMessage = context.bindings.playerBlobIn
+
+    }
+    else {
+
+        responseStatusCode = 404
+        responseHeaders = { "Content-Type": "text/plain" }
+        responseMessage = `No result found`
+
+    }
+
+    context.res = {
+        status: responseStatusCode,
+        headers: responseHeaders,
+        body: responseMessage
+    }
+
+}
+
+export default httpTrigger
---
+++ src/typescript/AzureFunctions.Blob/07CreatingABlobTriggeredFunction/HelloWorldBlobTrigger/index.ts
@@ -0,0 +1,7 @@
+import { AzureFunction, Context } from "@azure/functions"
+
+const blobTrigger: AzureFunction = async function (context: Context, myBlob: any): Promise<void> {
+    context.log("Blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
+};
+
+export default blobTrigger;
