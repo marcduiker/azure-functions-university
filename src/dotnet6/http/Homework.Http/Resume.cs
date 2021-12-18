179cc0b58c835f7f588e466085deb7435a37df96
Marc Duiker <marcduiker@gmail.com>
2021-12-18 22:29:29 +0100

    Add homework HTTP NET 6

---
+++ src/dotnet6/http/Homework.Http/Resume.cs
@@ -0,0 +1,11 @@
+namespace Homework.Http
+{
+    public class Resume
+    {
+        public string Name { get; set; }
+        public string Website { get; set; }
+        public string Country { get; set; }
+        public string Skills { get; set; }
+        public string CurrentRole { get; set; }
+    }
+}
