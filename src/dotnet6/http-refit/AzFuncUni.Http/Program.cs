c7bc81bc587eb104b15d30c2acbcf88ac0bfe08f
Maxime Labelle <springcomp@users.noreply.github.com>
2021-12-27 11:05:33 +0100

    [http-refit] Exercise #2 - Defining a third-party REST API.

--- src/dotnet6/http-refit/AzFuncUni.Http/Program.cs
+++ src/dotnet6/http-refit/AzFuncUni.Http/Program.cs
@@ -2,18 +2,38 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Azure.Functions.Worker.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Refit;
 
-namespace AzFuncUni.Http
+namespace src
 {
-    public class Program
-    {
-        public static void Main()
-        {
-            var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults()
-                .Build();
+	public class Program
+	{
+		public static void Main()
+		{
+			var builder = new HostBuilder()
+				.ConfigureFunctionsWorkerDefaults()
+				.ConfigureServices(ConfigureServices)
+				;
 
-            host.Run();
-        }
-    }
+			var host = builder.Build();
+
+			host.Run();
+		}
+
+		private const string HttpBinOrgApiHost = "http://httpbin.org";
+		private static void ConfigureServices(HostBuilderContext builder, IServiceCollection services)
+		{
+			services
+				.AddHttpClient("HttpBinOrgApi", (provider, client) =>
+				{
+					client.BaseAddress = new System.Uri(HttpBinOrgApiHost);
+					client.DefaultRequestHeaders.Add("Accept", "application/json");
+					client.DefaultRequestHeaders.Add("User-Agent", "dotnet-core/3.1");
+				})
+				.AddTypedClient(c => RestService.For<IHttpBinOrgApi>(c))
+				;
+		}
+	}
 }
