3acef7d12b2fd912f47bcec40fb8d847f328e449
Maxime LABELLE <maxime.labelle@chanel.com>
2022-01-03 17:49:33 +0100

    Made exercise 1 standalone.

--- src/dotnet6/http-refit/AzFuncUni.Http/HelloWorldHttpTrigger1.cs
+++ src/dotnet6/http-refit/AzFuncUni.Http/HelloWorldHttpTrigger1.cs
@@ -1,7 +1,5 @@
-using System;
+using System.Collections.Generic;
 using System.Net;
-using System.Threading.Tasks;
-using System.Web;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -17,34 +15,15 @@ namespace AzFuncUni.Http
             _logger = loggerFactory.CreateLogger<HelloWorldHttpTrigger1>();
         }
 
-        [Function(nameof(HelloWorldHttpTrigger1))]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        [Function("HelloWorldHttpTrigger1")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = default;
-            if (req.Method.Equals("get", StringComparison.OrdinalIgnoreCase))
-            {
-                var queryStringCollection = HttpUtility.ParseQueryString(req.Url.Query);
-                name = queryStringCollection["name"];
-            }
-            else
-            {
-                name = await req.ReadAsStringAsync();
-            }
-
-            var response = req.CreateResponse();
+            var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-            if (string.IsNullOrEmpty(name))
-            {
-                response.StatusCode = HttpStatusCode.BadRequest;
-                await response.WriteStringAsync($"Please provide a value for the name query string parameter or in the body as plain text.");
-            }
-            else
-            {
-                response.StatusCode = HttpStatusCode.OK;
-                await response.WriteStringAsync($"Hello, {name}");
-            }
+
+            response.WriteString("Welcome to Azure Functions!");
 
             return response;
         }
